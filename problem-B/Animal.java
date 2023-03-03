486b9eca47c6b9e9e051a628676b2674963d4d0f
Ulan220103157 <124434338+Ulan-777@users.noreply.github.com>
2023-03-04 03:35:03 +0600

    Update Animal.java

---
+++ problem-B/Animal.java
@@ -0,0 +1,12 @@
+public class Animal {
+    protected int legs = 8;
+    protected Animal(int legs){
+        this.legs = legs;
+    }
+    public void walk(){
+
+    }
+    public void eat(){
+
+    }
+}
